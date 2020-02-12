b051130a49d920147426882a1a6ecba8f37e5c02
Mesbaul Hasan <43781722+Mesbaulhasanbsmrstu@users.noreply.github.com>
2020-02-13 02:05:31 +0600

    update

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-
+import Firebase
 class RegisterViewController: UIViewController {
 
     
@@ -31,7 +31,16 @@ class RegisterViewController: UIViewController {
         
 
         
-        //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (User, error) in
+            if error != nil{
+                print(error!)
+            }
+                else  {
+                    print("successfully registration")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+                }
+            }
+        }
         
         
 
@@ -40,4 +49,4 @@ class RegisterViewController: UIViewController {
     } 
     
     
-}
+
